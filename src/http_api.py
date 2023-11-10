48c11e9546df5f03008aa208f1b128f62ea1d561
егор <egor@astra.com>
2023-11-10 15:32:16 +0300

    добавлен запуск сборка контенеров из compose

--- src/http_api.py
+++ src/http_api.py
@@ -1,16 +1,47 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+from enum import Enum
+
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'egor'}
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
-@app.get('/123')
-def get_roads():
-    "asdasdasd"
-    return {'Hello': '123123!'}
 
-# if __name__ == "__main__":
-#     uvicorn.run("http_api:app", host="0.0.0.0", port=5000, reload=True)
+@app.get('/', name='Это параметр name async', description='Это параметр description')
+async def read_root():
+    return {'Hello': 'asds'}
+
+
+@app.get('/items/{items_id}', name='Получить элемент. Тип не проверяем')
+async def read_item(items_id: int):
+    return {'item': items_id, 'message': 'Hello world'}
+
+
+@app.put('/items/{items_id}', name='Изменить элемент')
+def upgrade_item(items_id: int, item: Item):
+    return {'item_name': item.name, 'item_id': items_id}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lennet = "lennet"
+
+@app.get('/models/{model_name}', name='Enum эксперемент')
+async def enum_example(model_name: ModelName):
+    print('Model ', ModelName.alexnet.value)
+    if model_name is ModelName.alexnet:
+        return {'model_name': model_name, 'message': 'Deep Learning FTW'}
+    if model_name is ModelName.lennet:
+        return {'model_name': model_name, 'message': 'LeCNN all the image'}
+    return{'model_name': model_name, 'message': 'Have some residuals'}
+
+
+@app.get('/files/')
+async def getfile_path(skip: int, q: str | None = None, limit: bool = False):
+    return {'file_path', skip}
---
+++ start_docker.sh
@@ -0,0 +1,2 @@
+#docker run --name mp_kasud_ps -p 5000:5000 -v "$(pwd)"/src:/src mp_kasud_im
+docker start mp_kasud_ps
